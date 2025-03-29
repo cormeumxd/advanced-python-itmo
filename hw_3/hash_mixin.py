365b65eb2cf6d3f2da015a6b6741fa34d5571b2d
Artur Musaev <cormeum@MacBook-Air-Artur.local>
2025-03-29 12:07:30 +0300

    hw3 done

--- hw_3/hash_mixin.py
+++ hw_3/hash_mixin.py
@@ -1,5 +1,6 @@
 from typing import Iterable
 class HashMixin:
     def __hash__(self):
+        "Остаток от деления на 228 суммы всех элементов"
         hash_func = lambda x: sum(map(hash_func, x)) if isinstance(x, Iterable) else x
         return int(hash_func(self.matrix)) % 228
